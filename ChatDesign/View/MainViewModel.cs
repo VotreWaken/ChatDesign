d0077b740b50b1576e4bdf7ade5dd0418e465f12
Waken <votrevincentwaken@gmail.com>
2023-10-18 08:31:05 +0300

    Trying fix viewmodel

--- ChatDesign/View/MainViewModel.cs
+++ ChatDesign/View/MainViewModel.cs
@@ -16,6 +16,12 @@ using Data;
 using System.Data;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Controls;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace ChatDesign.View
 {
@@ -110,6 +116,7 @@ namespace ChatDesign.View
 
         #region Properties
         public ObservableCollection<ChatItem> MessagessItems { set; get; }
+        public ListBox Contacts { set; get; }
 
         public string SelectedUser
         {
@@ -181,7 +188,7 @@ namespace ChatDesign.View
         {
             ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 23016);
             user = new Data.User();
-            
+
             Users = new ObservableCollection<string>();
             Users.Add("Everyone");
             SelectedUser = Users.ElementAt(0);
@@ -266,7 +273,7 @@ namespace ChatDesign.View
             BinaryFormatter bf = new BinaryFormatter();
             while (true)
             {
-                
+
                 try
                 {
                     Message message = (Message)bf.Deserialize(nwStream);
@@ -321,6 +328,206 @@ namespace ChatDesign.View
                 SendData(new Message { Sender = user, ServerMessage = ServerMessage.RemoveUser });
             }
             catch { }
+        }
+
+
+
+        public ImageSource UserAvatar
+        {
+            set
+            {
+                UserAvatar = value;
+                Notify();
+            }
+            get => UserAvatar;
+        }
+
+        private void MyScrollViewer_ViewChanged(object sender, Control.MyScrollViewer e)
+        {
+            var scrollViewer = (ScrollViewer)sender;
+
+            // You can determine when to load more data based on your requirements
+            if (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
+            {
+                // Load more data here
+                //YourIncrementalLoadingCollectionInstance.LoadMoreItemsAsync(20); // Load 20 more items as an example
+            }
+        }
+        public MainViewModel(string username, byte[] image)
+        {
+
+
+            Bitmap bitmapavatar = GetImageFromByteArray(image);
+            UserAvatar = ImageSourceFromBitmap(bitmapavatar);
+
+            foreach (var item in DbOperations.GetUserChats(DbOperations.GetUserId(username)))
+            {
+                Bitmap bitmap = GetImageFromByteArray(item.Avatar);
+                Contacts.Items.Add(new CustomItem { ImagePath = ImageSourceFromBitmap(bitmap), Title = item.ChatName, Id = item.ID });
+                
+            }
+            SaveOriginalContacts();
+            // Initialize ChatMessages
+            ChatMessages = new ObservableCollection<ChatItem>();
+            Application.Current.MainWindow.Closing += new CancelEventHandler(MainWindow_Closing);
+        }
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            //MainViewModel viewModel = (MainViewModel)DataContext;
+            //MainViewModel viewModel = (MainViewModel)DataContext;
+            ChatItem i = new ChatItem { Content = TextMessage, IsSender = true, Sender = Username };
+            //ChatListBox.Items.Add(i);
+            ChatMessages.Add(i);
+        }
+        private void ContactItem_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Get the selected item
+            CustomItem selectedItem = (CustomItem)Contacts.SelectedItem;
+
+            // Check if an item is selected
+            if (selectedItem != null)
+            {
+                LoadMessages(((CustomItem)Contacts.SelectedItem).Id);
+                // Invoke the ConnectCommand with the selected item as a parameter
+                if (ConnectCommand.CanExecute(selectedItem))
+                {
+                    ConnectCommand.Execute(selectedItem);
+                }
+            }
+            //DataContext = this;
+            int chatID = ((CustomItem)Contacts.SelectedItem).Id;
+
+            // Load chat messages for the selected contact
+            ObservableCollection<ChatItem> chatMessages = DbOperations.LoadChatMessagesFromDatabase(chatID);
+
+            // Clear and update the ChatListBox
+            ChatMessages.Clear();
+            foreach (var message in chatMessages)
+            {
+                ChatMessages.Add(message);
+            }
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+        public Byte[] ReturnImagesByteArray()
+        {
+            return DbOperations.GetImages();
+        }
+
+        private static readonly ImageConverter _imageConverter = new ImageConverter();
+        public static Bitmap GetImageFromByteArray(byte[] byteArray)
+        {
+
+            Bitmap bm = (Bitmap)_imageConverter.ConvertFrom(byteArray);
+
+            if (bm != null && (bm.HorizontalResolution != (int)bm.HorizontalResolution ||
+                               bm.VerticalResolution != (int)bm.VerticalResolution))
+            {
+                // Correct a strange glitch that has been observed in the test program when converting 
+                //  from a PNG file image created by CopyImageToByteArray() - the dpi value "drifts" 
+                //  slightly away from the nominal integer value
+                bm.SetResolution((int)(bm.HorizontalResolution + 0.5f),
+                                 (int)(bm.VerticalResolution + 0.5f));
+            }
+
+            return bm;
+        }
+
+        public ObservableCollection<ChatItem> ChatMessages { get; set; } = new ObservableCollection<ChatItem>();
+
+        private List<CustomItem> originalItems = new List<CustomItem>();
+
+
+        private void SaveOriginalContacts()
+        {
+            for (int i = 0; i < Contacts.Items.Count; i++)
+            {
+                originalItems.Add((CustomItem)Contacts.Items[i]);
+            }
+        }
+
+        public string ContactsSearch
+        {
+            set
+            {
+                ContactsSearch = value;
+                Notify();
+            }
+            get => ContactsSearch;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = ContactsSearch; // Replace YourTextBox with your TextBox's name
+
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                Contacts.Items.Clear();
+                // If the search text is empty, no filtering is needed, so you can leave the original items as they are.
+                for (int i = 0; i < originalItems.Count; i++)
+                {
+                    Contacts.Items.Add((CustomItem)originalItems[i]);
+                }
+            }
+            else
+            {
+                // Create a filtered list to store the items that match the search text.
+                List<CustomItem> filteredItems = new List<CustomItem>();
+
+                foreach (CustomItem item in Contacts.Items)
+                {
+                    if (item.Title != null && item.Title.Contains(searchText))
+                    {
+                        filteredItems.Add(item);
+                    }
+                }
+
+                // Clear the existing items in the ListView.
+                Contacts.Items.Clear();
+
+                // Add the filtered items back to the ListView.
+                foreach (CustomItem item in filteredItems)
+                {
+                    Contacts.Items.Add(item);
+                }
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = ContactsSearch;
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                List<ChatDesign.Model.User> searchResults = DbOperations.SearchUsers(searchText);
+                MessageBox.Show(searchResults.Count.ToString());
+                // Update your UI (e.g., a ListView or ListBox) with the search results.
+                Contacts.Items.Clear();
+
+                foreach (ChatDesign.Model.User user in searchResults)
+                {
+                    Bitmap bitmap = GetImageFromByteArray(user.ImagePath);
+                    Contacts.Items.Add(new CustomItem { ImagePath = ImageSourceFromBitmap(bitmap), Title = user.Name });
+                }
+            }
+            else
+            {
+                // Handle the case when the search text is empty (e.g., show all users).
+                // You can reload the original list of users or display a message.
+            }
         }
 
     }
